8a425f67960a0e289ae3964f0d9638008ca04f4a
roman-morokhovskyi <roman_morokhovskyi@goodyear.com>
2023-03-27 10:36:57 +0300

    add constant image index

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import "./App.css";
-
 import React, { useState } from "react";
+import "./App.css";
+const MAX_IMAGE_AMOUNT = 53;
 
 const displayImages = () => {
   let count = 1;
   const images = [],
       basePath =
           "https://www.apple.com/105/media/us/airpods-pro/2022/d2deeb8e-83eb-48ea-9721-f567cf0fffa8/anim/hero/medium/00";
-  while (count <= 53) {
+  while (count <= MAX_IMAGE_AMOUNT) {
     let countNumber = count;
     if (count < 10) {
       countNumber = "0" + count;
@@ -27,21 +27,16 @@ const displayImages = () => {
 export default function App() {
   const [images] = useState(displayImages());
   const [imageIndex, setImageIndex] = useState(0);
-  const [imGalleryPos, setImGalleryPos] = useState("sticky");
   const [y, setY] = useState(0);
 
   const [scrollDirection, setScrollDirection] = useState("down");
+  const maxImageIndex = MAX_IMAGE_AMOUNT - 1;
 
   const onScrollHandler = (event) => {
     const scrollY = event.currentTarget.scrollTop;
 
     if (scrollY === 0) {
       setImageIndex(0);
-    }
-    if (imageIndex === 52 || scrollY >= 300) {
-      setImGalleryPos("relative");
-    } else {
-      setImGalleryPos("sticky");
     }
 
     if (y > scrollY) {
@@ -56,12 +51,10 @@ export default function App() {
 
   const showImage = () => {
     setImageIndex((prev) => {
-      console.log(prev, "prev");
-
       if (scrollDirection === "up" && !(prev <= 1)) {
         return prev - 1;
       }
-      if (scrollDirection === "down" && !(prev >= 52)) {
+      if (scrollDirection === "down" && !(prev >= maxImageIndex)) {
         return prev + 1;
       }
       return prev;
@@ -73,12 +66,12 @@ export default function App() {
       return ["visible"];
     }
 
-    return index === imageIndex ? ["visible"] : ["image"];
+    return index === imageIndex ? "visible" : "image";
   };
 
   return (
       <div className="App" onScroll={onScrollHandler}>
-        <div id="image-container" style={{ position: imGalleryPos }}>
+        <div id="image-container">
           {images.map((im) => (
               <img className={getClassName(im.index)} src={im.src} alt={im.alt} />
           ))}
