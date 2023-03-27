90e32329794ab874e5ab1e199e0db15687190dd3
roman-morokhovskyi <roman_morokhovskyi@goodyear.com>
2023-03-27 10:19:46 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+import React, { useState } from "react";
+
+const displayImages = () => {
+  let count = 1;
+  const images = [],
+      basePath =
+          "https://www.apple.com/105/media/us/airpods-pro/2022/d2deeb8e-83eb-48ea-9721-f567cf0fffa8/anim/hero/medium/00";
+  while (count <= 53) {
+    let countNumber = count;
+    if (count < 10) {
+      countNumber = "0" + count;
+    }
+    images.push({
+      src: `${basePath}${countNumber}.png`,
+      class: "image",
+      alt: count,
+      index: count
+    });
+    count++;
+  }
+
+  return images;
+};
+
+export default function App() {
+  const [images] = useState(displayImages());
+  const [imageIndex, setImageIndex] = useState(0);
+  const [imGalleryPos, setImGalleryPos] = useState("sticky");
+  const [y, setY] = useState(0);
+
+  const [scrollDirection, setScrollDirection] = useState("down");
+
+  const onScrollHandler = (event) => {
+    const scrollY = event.currentTarget.scrollTop;
+
+    if (scrollY === 0) {
+      setImageIndex(0);
+    }
+    if (imageIndex === 52 || scrollY >= 300) {
+      setImGalleryPos("relative");
+    } else {
+      setImGalleryPos("sticky");
+    }
+
+    if (y > scrollY) {
+      setScrollDirection("up");
+    } else if (y < scrollY) {
+      setScrollDirection("down");
+    }
+    setY(scrollY);
+
+    showImage();
+  };
+
+  const showImage = () => {
+    setImageIndex((prev) => {
+      console.log(prev, "prev");
+
+      if (scrollDirection === "up" && !(prev <= 1)) {
+        return prev - 1;
+      }
+      if (scrollDirection === "down" && !(prev >= 52)) {
+        return prev + 1;
+      }
+      return prev;
+    });
+  };
+
+  const getClassName = (index) => {
+    if (index === 1 && imageIndex === 0) {
+      return ["visible"];
+    }
+
+    return index === imageIndex ? ["visible"] : ["image"];
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App" onScroll={onScrollHandler}>
+        <div id="image-container" style={{ position: imGalleryPos }}>
+          {images.map((im) => (
+              <img className={getClassName(im.index)} src={im.src} alt={im.alt} />
+          ))}
+        </div>
+        {[...Array(100)].map((_, index) => (
+            <p key={index}>
+              <br />
+            </p>
+        ))}
+      </div>
   );
 }
-
-export default App;
